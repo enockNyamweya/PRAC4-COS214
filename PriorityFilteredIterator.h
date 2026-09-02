277f384197030a1e40ec9434fa416c5e7979a3a7
Showlon <u25502469@tuks.co.za>
2026-09-02 18:56:31 +0200

    Definitions of functions included

--- PriorityFilteredIterator.h
+++ PriorityFilteredIterator.h
@@ -1,4 +1,16 @@
 #ifndef PRIORITYFILTEREDITERATOR_H
 #define PRIORITYFILTEREDITERATOR_H
 
+#include "WorkIterator.h"
+
+class PriorityFilteredIterator : public WorkIterator{
+    private:
+        WorkIterator* inneriterator;
+    public:
+        void first();
+        void next();
+        bool isDone();
+        WorkUnit* currentItem();
+};
+
 #endif
