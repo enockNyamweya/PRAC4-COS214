008a039df02431ca9fe6c378464f047af43bfa7c
Showlon <u25502469@tuks.co.za>
2026-09-02 18:38:46 +0200

    Definitions of functions included

--- PackageDecorator.h
+++ PackageDecorator.h
@@ -1,4 +1,16 @@
 #ifndef PACKAGEDECORATOR_H
 #define PACKAGEDECORATOR_H
 
+#include "WorkUnit.h"
+#include "WorkIterator.h"
+
+class PackageDecorator : public WorkUnit {
+    private:
+        WorkUnit* wrappedUnit;
+    public:
+        virtual void execute() = 0;
+        virtual double getWeight() = 0;
+        virtual WorkIterator* createIterator() = 0;   
+};
+
 #endif
