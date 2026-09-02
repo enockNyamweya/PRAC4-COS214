9e2a4edbf09e53d381ca6c96d7456b18cdef4052
Showlon <u25502469@tuks.co.za>
2026-09-02 18:43:25 +0200

    Definitions of functions included

--- InsuranceDecorator.h
+++ InsuranceDecorator.h
@@ -1,4 +1,17 @@
 #ifndef INSURANCEDECORATOR_H
 #define INSURANCEDECORATOR_H
 
+#include <string>
+using namespace std;
+
+#include "PackageDecorator.h"
+
+class InsuranceDecorator : public PackageDecorator{
+    private:
+        double CoverageAmount;
+        string policyNumber;
+    public:
+        void execute();    
+};
+
 #endif
