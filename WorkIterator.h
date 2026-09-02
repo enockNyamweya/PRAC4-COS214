1d7ccc45d44305f8d380b65bf201e9f77742534d
Showlon <u25502469@tuks.co.za>
2026-09-02 18:46:11 +0200

    Definitions of functions included

--- WorkIterator.h
+++ WorkIterator.h
@@ -1,4 +1,14 @@
 #ifndef WORKITERATOR_H
 #define WORKITERATOR_H
 
+#include "WorkUnit.h"
+
+class WorkIterator{
+    public:
+        virtual void first() = 0;
+        virtual void next() = 0;
+        virtual bool isDone() = 0;
+        virtual WorkUnit* currentItem() = 0;
+};
+
 #endif
