82b74aee13edd98f6e63d9d366543bd64d2c6e2f
Showlon <u25502469@tuks.co.za>
2026-09-02 18:41:11 +0200

    Definitions of functions included

--- ExpressShippingDecorator.h
+++ ExpressShippingDecorator.h
@@ -1,4 +1,14 @@
 #ifndef EXPRESSSHIPPINGDECORATOR_H
 #define EXPRESSSHIPPINGDECORATOR_H
 
+#include "PackageDecorator.h"
+
+class ExpressShippingDecorator : public PackageDecorator{
+    private:
+        double expressFee;
+        int priorityLevel;
+    public:
+        void execute();    
+};
+
 #endif
