b5c85d5d658b06d1a0d2e1bff7bdda066287b650
Showlon <u25502469@tuks.co.za>
2026-09-02 18:49:32 +0200

    Definitions of functions included

--- SnapshotIterator.h
+++ SnapshotIterator.h
@@ -1,4 +1,21 @@
 #ifndef SNAPSHOTITERATOR_H
 #define SNAPSHOTITERATOR_H
 
+#include <vector>
+
+#include "WorkIterator.h"
+
+using namespace std;
+
+class SnapshotIterator : public WorkIterator{
+    private:
+        vector<WorkUnit*> snapshot;
+        int index;
+    public:
+        void first();
+        void next();
+        bool isDone();
+        WorkUnit* currentItem();    
+};
+
 #endif
