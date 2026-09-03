2b1cebec7645e8c02e0df8ccaca084a20f561e07
Showlon <u25502469@tuks.co.za>
2026-09-03 23:27:57 +0200

    Implementation

---
+++ SnapshotIterator.cpp
@@ -0,0 +1,40 @@
+#include "SnapshotIterator.h"
+
+SnapshotIterator::SnapshotIterator(const vector<WorkUnit *> &items)
+{
+    snapshot = items;
+    index = 0;
+}
+
+void SnapshotIterator::first()
+{
+    index = 0;
+}
+
+void SnapshotIterator::next()
+{
+    if(!isDone()){
+        index++;
+    }
+}
+
+bool SnapshotIterator::isDone()
+{
+    return index >= static_cast<int>(snapshot.size());
+}
+
+WorkUnit *SnapshotIterator::currentItem()
+{
+    if(isDone()){
+        return nullptr;
+    }
+    return snapshot[index];
+}
+
+SnapshotIterator::~SnapshotIterator()
+{
+    for(WorkUnit* ptr : snapshot){
+        delete ptr;
+    }
+    snapshot.clear();
+}
